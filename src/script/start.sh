786c04b1356ecaacc634c5065ca2f5459ed96f3a
test-aws-lab <test@azure-dns-test.com>
2026-07-22 14:03:51 +0900

    メッセージ

--- src/script/start.sh
+++ src/script/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+# Apacheの停止
+sudo systemctl stop httpd
+
 # 権限を付与してバックグラウンドで起動
 chmod +x /opt/my-app/my-app
 nohup /opt/my-app/my-app > /var/log/my-app.log 2>&1 &
 
-# Apacheの停止
-sudo systemctl stop httpd
